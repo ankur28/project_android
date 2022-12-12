0d012f46ac6c13e400212c971f1c61e06fb4d68d
ankur28 <ankurkalson28@gmail.com>
2022-12-11 22:13:33 -0500

    map and menu options added

--- app/src/main/java/com/example/projectandroid/MapsActivity.kt
+++ app/src/main/java/com/example/projectandroid/MapsActivity.kt
@@ -1,7 +1,18 @@
 package com.example.projectandroid
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,14 +21,23 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.projectandroid.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.GroundOverlayOptions
+import java.io.IOException
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,21 +48,116 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        enableMyLocation()
+        mMap.uiSettings.setZoomControlsEnabled(true)
+        mMap.setTrafficEnabled(true)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    // creating a menu to change map type
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        // Change the map type based on the user's selection.
+        R.id.gMap -> {
+            Toast.makeText(this@MapsActivity, "Maps selected", Toast.LENGTH_SHORT).show()
+
+            true
+        }
+        R.id.gPlaces -> {
+            Toast.makeText(this@MapsActivity, "Places selected", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.email -> {
+            Toast.makeText(this@MapsActivity, "Email selected", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.about -> {
+            Toast.makeText(this@MapsActivity, "About selected", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun getAddress(loc:LatLng): String? {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        var addresses: List<Address>? = null
+        try {
+            addresses = geocoder.getFromLocation(loc!!.latitude, loc!!.longitude, 1)
+        } catch (e1: IOException) {
+            Log.e("Geocoding", getString(R.string.problem), e1)
+        } catch (e2: IllegalArgumentException) {
+            Log.e("Geocoding", getString(R.string.invalid)+
+                    "Latitude = " + loc!!.latitude +
+                    ", Longitude = " +
+                    loc!!.longitude, e2)
+        }
+        // If the reverse geocode returned an address
+        if (addresses != null) {
+            // Get the first address
+            val address = addresses[0]
+            val addressText = String.format(
+                "%s, %s, %s",
+                address.getAddressLine(0),
+                address.locality,
+                address.countryName)
+            return addressText
+        }
+        else
+        {
+            Log.e("Geocoding", getString(R.string.noaddress))
+            return ""
+        }
+    }
+
+    // Location services permission
+    // this will result in a blue dot being your current location
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+
+            }
+        }
+    }
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            mMap.isMyLocationEnabled = true
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                it.apply {
+
+                    var current_location = LatLng(latitude,longitude)
+                    var address =    getAddress(current_location)
+                    mMap.addMarker(MarkerOptions().position(current_location)
+                        .title("Ankur Kalson's current location")
+                        .snippet(address))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(current_location))
+
+                }
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
     }
 }
